3a40fb6f1c16532e0d9bf6ee35c373a807ddca32
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 20:39:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,4 +20,11 @@ attr_accessor :sender, :receiver, :amount, :status
       return "Transaction rejected. Please check your account balance."
     end
   end
- 
+ def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+end
