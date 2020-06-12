10da0768096c4048196546c98f4b30e18b354d24
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 20:26:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+attr_accessor  :reciver, :status 
+attr_reader :sender
+def initialize(sender)
+@sender = sender 
+@reciver= reciver 
+@status = "pending"
+
+end 
+  
+  
 end
